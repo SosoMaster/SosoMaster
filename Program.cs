b92a8f5cd512ef9efd129fe636b66c4d9de40d83
Alejandro Garcia <161458542+SosoMaster@users.noreply.github.com>
2024-03-05 12:42:38 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp30 {
+    internal class Program {
+        static void Main(string[] args) 
+        {
+            string comand1 = Console.ReadLine();
+            string comand2 = Console.ReadLine();
+            string comand3 = Console.ReadLine();
+
+
+            string[] Char = { comand1,  comand2, comand3,}; ;
+
+            for(int i = 0; i < Char.Length; i++) 
+            {
+                Console.WriteLine(Char[i]);
+            }
+        }
+    }
+}
