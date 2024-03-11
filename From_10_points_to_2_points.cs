2ddc61c9e58bdbc305f4ed8f936024ee6f133cfa
Alexander Gatev <161458542+SosoMaster@users.noreply.github.com>
2024-03-11 21:49:53 +0200

    Add files via upload

---
+++ From_10_points_to_2_points.cs
@@ -0,0 +1,33 @@
+﻿namespace Attempt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter decimal number: ");
+            int deci = int.Parse(Console.ReadLine());
+
+            List<int> binaryDigits = new List<int>();
+
+            if (deci == 0)
+            {
+                binaryDigits.Add(0);
+            }
+            else
+            {
+                while (deci > 0)
+                {
+                    binaryDigits.Add(deci % 2);
+                    deci /= 2;
+                }
+            }
+
+            Console.Write("Binary representation: ");
+            for (int i = binaryDigits.Count - 1; i >= 0; i--)
+            {
+                Console.Write(binaryDigits[i]);
+            }
+            Console.WriteLine(); ;
+        }
+    }
+}
